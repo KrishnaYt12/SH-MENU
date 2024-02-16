42a1600a456fff6ccae8e241db9368ee4b745128
KrishnaYt12 <124163702+KrishnaYt12@users.noreply.github.com>
2024-02-17 02:35:57 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SH-HUB'
+description 'qb-menu CSS Edited By SH-HUB'
+version '1.6.9'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css'
+}
+
+lua54 'yes'
